d1ce926019e5ec454d2aacb261a7134705ccee8c
Błażej Ostrowski <15blazi155@gmail.com>
2026-05-09 12:48:27 +0200

    init project

---
+++ backend-java/README.md
@@ -0,0 +1,1 @@
+# Urban-Heat-Intelligence-Platform---java-backend
---
+++ backend-java/src/main/java/com/virtualmanufactory/SpringBootUrbanHeatApplication.java
@@ -0,0 +1,12 @@
+package com.virtualmanufactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUrbanHeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUrbanHeatApplication.class, args);
+	}
+}
